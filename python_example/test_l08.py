04176acf97b356697222660149f1493374920e17
NovikovII <ivan.novikov@moex.com>
2017-02-28 13:10:56 +0300

    Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров

--- python_example/test_l08.py
+++ python_example/test_l08.py
@@ -17,9 +17,9 @@ def driver(request):
 def test_l08(driver):
     driver.get("http://localhost/litecart/")
     driver.implicitly_wait(10)
-    driver.find_element_by_css_selector(".image-wrapper")
+    driver.find_element_by_css_selector("li.product")
 
-    popular_products = driver.find_elements_by_css_selector(".image-wrapper")
+    popular_products = driver.find_elements_by_css_selector("li.product")
 
     for popular_product in popular_products:
         stickers = popular_product.find_elements_by_css_selector(".sticker")
