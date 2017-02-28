7a5586cca07ef07841446e3d96527a03f32d639c
NovikovII <ivan.novikov@moex.com>
2017-02-28 16:38:53 +0300

    Задание 9. Проверить сортировку стран и геозон в админке (часть первая)

---
+++ python_example/test_l09.py
@@ -0,0 +1,42 @@
+__author__ = 'NovikovII'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver(request):
+    driver = webdriver.Firefox()
+    request.addfinalizer(driver.quit)
+    return driver
+
+
+def test_l09_1(driver):
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    wait = WebDriverWait(driver, 10)
+
+    countries = [i.text for i in driver.find_elements_by_css_selector("tr.row a:not([title])")]
+    assert (countries == sorted(countries))
+
+    #index = [[i.text for i in driver.find_elements_by_css_selector("td:nth-child(6)")].index(i) for i in [i.text for i in driver.find_elements_by_css_selector("td:nth-child(6)")] if i != '0']
+    not_zero_zones = [i.text for i in driver.find_elements_by_css_selector("td:nth-child(6)")]
+    not_zero_zones_indexs = [not_zero_zones.index(i) for i in not_zero_zones if i != '0']
+
+    for not_zero_zones_index in not_zero_zones_indexs:
+        country = driver.find_elements_by_css_selector("td:nth-child(5)>a")
+        country[not_zero_zones_index].click()
+        zones = [i.get_attribute("textContent") for i in driver.find_elements_by_css_selector("#table-zones tr>td:nth-child(3)") if i.get_attribute("textContent") != ""]
+        assert (zones == sorted(zones))
+        driver.back()
+
+
+def test_l09_2(driver):
+    pass
